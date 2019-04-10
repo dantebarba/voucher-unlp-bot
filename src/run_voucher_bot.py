8f6601f746fa24543b709bfa1472c0047c2ac1c6
Dante Barba <dantebarba@gmail.com>
2019-04-10 20:53:19 -0300

    parametrized Api Url

--- src/run_voucher_bot.py
+++ src/run_voucher_bot.py
@@ -4,4 +4,5 @@ import voucher_bot
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ejecucion del bot servicio')
     parser.add_argument('--token',  help='el token del bot')
-    voucher_bot.VoucherBot(parser.parse_args().token).start_pooling()
+    parser.add_argument('--api',  help='la url de la API')
+    voucher_bot.VoucherBot(parser.parse_args().token, parser.parse_args().api).start_pooling()
--- src/voucher_bot.py
+++ src/voucher_bot.py
@@ -8,10 +8,10 @@ logging.basicConfig(level=logging.ERROR,
 
 class VoucherBot():
     ''' Esta clase contiene todo lo necesario para operar el bot '''
-    _url = "http://localhost:5000/voucher?url=http://lafuenteunlp.com.ar/web/"
 
-    def __init__(self, token):
+    def __init__(self, token, api_url):
         self._updater = Updater(token=token)
+        self._url = api_url
         self._dispatcher = self._updater.dispatcher
         self.start_handler = CommandHandler('start', self.start)
         self.voucher_handler = CommandHandler('voucher', self.voucher)
@@ -25,10 +25,13 @@ class VoucherBot():
     
     def voucher(self, update, context):
         ''' Devuelve el voucher '''
-        context.bot.send_message(chat_id=update.message.chat_id, text=VoucherHttpRetriever.retrieve(self._url))
+        context.bot.send_message(chat_id=update.message.chat_id, text=VoucherHttpRetriever.retrieve(self.api_url()))
 
     def start_pooling(self):
         self._updater.start_polling()
+
+    def api_url(self):
+        return self._url + "/voucher?url=http://lafuenteunlp.com.ar/web/"
 
 class VoucherHttpRetriever():
     ''' recupera el voucher mediante http '''
